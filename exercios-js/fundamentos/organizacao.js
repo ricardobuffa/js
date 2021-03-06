aa2c6e35a1f7248eb868a042248b66c765ea89e9
Ricardo Buffa <ricardobuffa@gmail.com>
2021-03-06 09:15:07 -0300

    comitando o projeto do vs

---
+++ exercios-js/fundamentos/organizacao.js
@@ -0,0 +1,27 @@
+console.log(
+    "Sentença de código")
+//Comentários sobre pular linhas
+/*
+No javascript não pode pular linha onde quiser o de cima está certo
+O exemplo abaixo está incorreto
+console.log("Sentença
+             de código")
+
+*/
+
+
+//Comentários sobre blocos
+/*
+No javascript os blocos podem ser abertos sem precisar de um comando como no exemplo abaixo
+Note também que o console.log aceita aspas duplas ou plica (aspas simples)
+Pular a linha é padrão no console.log sem precisar do \n
+*/
+
+{
+    {
+        console.log("Olá")
+        console.log('Mundo')
+
+    }
+
+}
